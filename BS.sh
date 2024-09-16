82de273b21c7e9683ded7e66f7deea6a3401a025
Augustun1000 <104395373+Augustun1000@users.noreply.github.com>
2024-09-16 17:23:22 -0300

    Update BS.sh

--- BS.sh
+++ BS.sh
@@ -2,7 +2,9 @@
 #Copyright (C) 2023 Augustun1000 <augustoperezriesgo@gmail.com>
 
 # List of packages to install
-sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xrandr xdg-user-dirs xf86-video-intel mesa lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-tools yad arandr gvfs htop neofetch pcmanfm-gtk3 lm_sensors lxappearance-gtk3 nano geany rofi p7zip flameshot xarchiver meld mplayer ttf-dejavu ttf-liberation noto-fonts-cjk pulsemixer mtpfs gvfs-mtp gvfs-gphoto2 os-prober ncdu chromium
+#Active multilib
+
+sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xrandr xdg-user-dirs xf86-video-intel mesa lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-tools yad arandr gvfs htop neofetch pcmanfm-gtk3 lm_sensors lxappearance-gtk3 nano geany rofi p7zip flameshot xarchiver meld mplayer ttf-dejavu ttf-liberation noto-fonts-cjk pulsemixer mtpfs gvfs-mtp gvfs-gphoto2 os-prober ncdu chromium gparted xorg-xev
 
 
 #Universal intel drivers (All gen 1 to up)
